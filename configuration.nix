ed364211103a9d50a26a82ac3367eced8a10a513
only1thor <>
2023-12-25 23:21:59 +0100

    set boot menu generations limit

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,7 @@
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
