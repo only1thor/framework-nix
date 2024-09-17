cdb2f7f06cf00c1d754b1b2f3014229f29ed2f85
only1thor <>
2024-09-17 09:44:03 +0200

    revert nixpkgs, bump hardware, add more boot states for tests
    Something in nixpkgs results in my framework13 intel gen12 loosing wifi,
    reverting to last working input for nixpkgs.
    Bumping the nix-hardware, as it seems to have no impact on the wifi.
    adding more boot generations, so i test without wiping the last working config.

--- flake.nix
+++ flake.nix
@@ -8,20 +8,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs"; # Ensure we're using the same nixpkgs version
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager,... }@inputs: rec{
-    # This instantiates nixpkgs for each system listed
-    # Allowing you to configure it (e.g. allowUnfree)
-    # Our configurations will use these instances
-    legacyPackages = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" ] (system:
-      import inputs.nixpkgs {
-        inherit system;
-        # NOTE: Using `nixpkgs.config` in your NixOS config won't work
-        # Instead, you should set nixpkgs configs here
-        # (https://nixos.org/manual/nixpkgs/stable/#idm140737322551056)
-
-        config.allowUnfree = true;
-      }
-    );
+  outputs = { self, nixpkgs, nixos-hardware, home-manager,... }:{
     nixosConfigurations = {
       framework = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
